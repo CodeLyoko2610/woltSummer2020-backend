f950a857b32e9252a0ec5e8ee69b0c502f726279
chuongcuu <dangchuong1999@outlook.com>
2020-02-05 14:02:11 +0200

    Add validation for user request | Take in user input | Calc distance from user to restaurant and return results.

--- functions/funcs.js
+++ functions/funcs.js
@@ -1,6 +1,45 @@
+//Import modules
+const config = require('config');
+
 module.exports = {
-    calcDistance: (lat, lon) => {
-        console.log(lat);
-        console.log(lon);
+  calcDistance: (lon1, lat1, lon2, lat2, restaurant) => {
+    //Destruct restaurant
+    let {
+      name
+    } = restaurant;
+    //Using Haversinne Formula
+    let p = Math.PI / 180;
+    let dLon = p * (lon2 - lon1);
+    let dLat = p * (lat2 - lat1);
+
+    let a =
+      Math.pow(Math.sin(dLat / 2), 2) +
+      Math.cos(p * lat1) * Math.cos(p * lat2) * Math.pow(Math.sin(dLon / 2), 2);
+
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    let d = 6371 * c;
+
+    if (d <= 3) {
+      return {
+        d,
+        name
+      };
+    } else {
+      return null
     }
-}
+    // console.log('p: ' + p + ' ' + typeof p);
+    // console.log('dLon: ' + dLon + ' ' + typeof dLon);
+    // console.log('dLat: ' + dLat + ' ' + typeof dLat);
+    // console.log('a: ' + a + ' ' + typeof a);
+    // console.log('c: ' + c + ' ' + typeof c);
+    // console.log('d: ' + d + ' ' + typeof d);
+  },
+
+  calcDistance1: (lat, lon) => {
+    console.log(lat);
+    console.log(lon);
+    console.log(config.get('R'));
+  }
+};
+
+//https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
--- routes/api/restaurants.js
+++ routes/api/restaurants.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const {
-    check,
-    param,
-    validationResult
+  check,
+  param,
+  validationResult
 } = require('express-validator');
 
 //Bring in model
@@ -16,136 +16,159 @@ const functions = require('../../functions/funcs.js');
 //@desc Test route
 //@access Public
 router.get('/', (req, res) => {
-    console.dir(req.body);
-    res.send('Request received.');
+  console.dir(req.body);
+  res.send('Request received.');
 });
 
 //@route POST api/restaurants/
 //@desc Test route
 //@access Public
 router.post(
-    '/',
-    [
-        check('blurhash').isString(),
-        check('city').isString(),
-        check('currency').isString(),
-        check('delivery_price').isNumeric(),
-        check('description').isString(),
-        check('image').isString(),
-        check('location').isArray(),
-        check('name').isString(),
-        check('online').isBoolean(),
-        check('tags').isArray()
-    ],
-    async (req, res) => {
-        let errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({
-                errors: errors.array()
-            });
-        }
-
-        let {
-            blurhash,
-            city,
-            currency,
-            delivery_price,
-            description,
-            image,
-            location,
-            name,
-            online,
-            tags
-        } = req.body;
-
-        try {
-            let newRestaurant = await Restaurant.findOne({
-                blurhash
-            });
-
-            if (newRestaurant) {
-                return res.status(400).json({
-                    errors: [{
-                        msg: 'Restaurant already exists.'
-                    }]
-                });
-            }
-
-            newRestaurant = new Restaurant({
-                blurhash,
-                city,
-                currency,
-                delivery_price,
-                description,
-                image,
-                location,
-                name,
-                online,
-                tags
-            });
-
-            await newRestaurant.save();
-
-            res.send(newRestaurant);
-        } catch (error) {
-            console.error(error.message);
-            res.status(500).send('Server error.');
-        }
+  '/',
+  [
+    check('blurhash').isString(),
+    check('city').isString(),
+    check('currency').isString(),
+    check('delivery_price').isNumeric(),
+    check('description').isString(),
+    check('image').isString(),
+    check('location').isArray(),
+    check('name').isString(),
+    check('online').isBoolean(),
+    check('tags').isArray()
+  ],
+  async (req, res) => {
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array()
+      });
     }
+
+    let {
+      blurhash,
+      city,
+      currency,
+      delivery_price,
+      description,
+      image,
+      location,
+      name,
+      online,
+      tags
+    } = req.body;
+
+    try {
+      let newRestaurant = await Restaurant.findOne({
+        blurhash
+      });
+
+      if (newRestaurant) {
+        return res.status(400).json({
+          errors: [{
+            msg: 'Restaurant already exists.'
+          }]
+        });
+      }
+
+      newRestaurant = new Restaurant({
+        blurhash,
+        city,
+        currency,
+        delivery_price,
+        description,
+        image,
+        location,
+        name,
+        online,
+        tags
+      });
+
+      await newRestaurant.save();
+
+      res.send(newRestaurant);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server error.');
+    }
+  }
 );
 
 //@route POST api/restaurants/search/q={}&lat={}&lon={}
 //@desc Query route
 //@access Public
-router.post('/search/q=:q&lat=:lat&lon=:lon', [
+router.post(
+  '/search/q=:q&lon=:lon&lat=:lat',
+  [
     param('q').isString(),
     param('q').isLength({
-        min: 1
+      min: 1
     }),
     param('lat').isNumeric(),
     param('lon').isNumeric()
-], async (req, res) => {
+  ],
+  async (req, res) => {
     try {
-        let errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({
-                errors: errors.array()
-            })
-        }
+      let errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array()
+        });
+      }
 
-        let {
-            q,
+      let {
+        q,
+        lon,
+        lat
+      } = req.params;
+      let resultsArr = [];
+      //Search using $text operator
+      let results = await Restaurant.find({
+        $text: {
+          $search: q
+        }
+      });
+
+      if (results.length === 0) {
+        return res.status(400).json({
+          errors: [{
+            msg: 'Sorry. No restaurant found.'
+          }]
+        }); //No result
+      } else if (results.length === 1) {
+        resultsArr.push(
+          functions.calcDistance(
+            lon,
             lat,
-            lon
-        } = req.params;
+            results.location[0],
+            results.location[1],
+            results
+          )
+        ); //Only 1 result, 1 object received
+      } else {
+        results.forEach(result => {
+          resultsArr.push(
+            functions.calcDistance(
+              lon,
+              lat,
+              result.location[0],
+              result.location[1],
+              result
+            )
+          ); //Many results, array of objs received
+        });
+      }
 
-        //Search using $text operator
-        let results = await Restaurant.findOne({
-            $text: {
-                $search: q
-            }
-        })
-
-
-        if (!results) {
-            return res.status(400).json({
-                errors: [{
-                    msg: 'No restaurant found.'
-                }]
-            }) //No result
-        } else if (results && !results.length) {
-            functions.calcDistance(results.location[0], results.location[1]) //Only 1 result, object received
-        } else {
-            results.forEach(result => {
-                functions.calcDistance(result.location[0], result.location[1]) //Many results, array of obj received
-            })
-        }
-
-        res.json(results);
+      res.json({
+        resultsArr,
+        results
+      })
+      //res.send("This is length: " + results.length);
     } catch (error) {
-        console.error(error.message);
-        res.status(500).send('Server error.');
+      console.error(error.message);
+      res.status(500).send('Server error.');
     }
-});
+  }
+);
 
 module.exports = router;
